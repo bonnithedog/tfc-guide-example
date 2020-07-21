6f2fc7f24c391029b7f001a364669be607514bd1
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-21 22:02:31 +0200

    Revert "Update main.tf"
    This reverts commit 5cc4e5d43e09c6aa58154d174e415a73087259be.

--- main.tf
+++ main.tf
@@ -1,19 +1,28 @@
-provider "azurerm" {
-  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
-  version = "=1.44.0"
+provider "aws" {
+  version = "2.33.0"
 
-  subscription_id = "5f821010-caae-4a0c-b435-0bc6224fea93"
-
-  tenant_id = "147ae4e2-d977-491a-9760-680bc8ce94c9"
+  region = var.aws_region
 }
 
-resource "azurerm_resource_group" "itmanaged" {
-  name     = "itmanagerResourceGroup1"
-  location = "NorthEurope"
+provider "random" {
+  version = "2.2"
+}
+
+resource "random_pet" "table_name" {}
+
+resource "aws_dynamodb_table" "tfc_example_table" {
+  name = "${var.db_table_name}-${random_pet.table_name.id}"
+
+  read_capacity  = var.db_read_capacity
+  write_capacity = var.db_write_capacity
+  hash_key       = "UUID"
+
+  attribute {
+    name = "UUID"
+    type = "S"
+  }
 
   tags = {
-    environment = "Production"
+    user_name = var.tag_user_name
   }
 }
-
-
