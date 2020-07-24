77ea7911e32dc06590b99942e0cfa6a9a9d04270
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-24 10:08:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,46 @@
-provider "aws" {
-  version = "2.33.0"
+#provider "aws" {
+#  version = "2.33.0"
+#
+#  region = var.aws_region
+#}
+#
+#provider "random" {
+#  version = "2.2"
+#}
+#
+#resource "random_pet" "table_name" {}
+#
+#resource "aws_dynamodb_table" "tfc_example_table" {
+#  name = "${var.db_table_name}-${random_pet.table_name.id}"
+#
+#  read_capacity  = var.db_read_capacity
+#  write_capacity = var.db_write_capacity
+#  hash_key       = "UUID"
+#
+#  attribute {
+#    name = "UUID"
+#    type = "S"
+#  }
+#
+#  tags = {
+#    user_name = var.tag_user_name
+#  }
+#}
+provider "azurerm" {
+  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
+  version = "=1.44.0"
 
-  region = var.aws_region
+  subscription_id = "*************"
+
+  tenant_id = "*********************"
 }
-
-provider "random" {
-  version = "2.2"
-}
-
-resource "random_pet" "table_name" {}
-
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
-
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-
-  attribute {
-    name = "UUID"
-    type = "S"
-  }
-
-  tags = {
-    user_name = var.tag_user_name
-  }
-}
+#
+#resource "azurerm_resource_group" "itmanaged" {
+#  name     = "itmanagerResourceGroup1"
+#  location = "NorthEurope"
+#
+#  tags = {
+#    environment = "Production"
+#  }
+#}
+#
