8e2b58f6854e15d09691210942a24ece18c385fa
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-24 21:13:57 +0200

    Update storagetmp.tf

--- storagetmp.tf
+++ storagetmp.tf
@@ -11,7 +11,7 @@ resource "random_id" "randomId" {
 # Create storage account for boot diagnostics
 resource "azurerm_storage_account" "nagios_storageaccount" {
     name                        = "diag${random_id.randomId.hex}"
-    resource_group_name         = "${azurerm_resource_group.nagios_resourcegroup.name}"
+    resource_group_name         = azurerm_resource_group.rg001a.name
     location                    = "northeurope"
     account_tier                = "Standard"
     account_replication_type    = "LRS"
