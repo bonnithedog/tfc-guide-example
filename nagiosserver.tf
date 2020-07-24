724a4d1bbf28f63894d8cffc98a9e9ae557a73c3
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-24 21:29:00 +0200

    Update nagiosserver.tf

--- nagiosserver.tf
+++ nagiosserver.tf
@@ -3,7 +3,7 @@
 resource "azurerm_public_ip" "nagios_publicip" {
     name                         = "nagios_PublicIP"
     location                     = "northeurope"
-    resource_group_name          = "${azurerm_resource_group.rg001a.name}"
+    resource_group_name          = azurerm_resource_group.nagios_resourcegroup.name
     allocation_method            = "Static"
 
     #tags {
@@ -16,14 +16,14 @@ resource "azurerm_public_ip" "nagios_publicip" {
 resource "azurerm_network_interface" "nagios_nic" {
     name                      = "nagios_NIC"
     location                  = "northeurope"
-    resource_group_name       = "${azurerm_resource_group.rg001a.name}"
-    network_security_group_id = "${azurerm_network_security_group.nagios_sg.id}"
+    resource_group_name       = azurerm_resource_group.nagios_resourcegroup.name
+    network_security_group_id = azurerm_network_security_group.nagios_sg.id
 
     ip_configuration {
         name                          = "nagios_NICConfiguration"
-        subnet_id                     = "${azurerm_subnet.nagios_subnet.id}"
+        subnet_id                     = azurerm_subnet.nagios_subnet.id
         private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = "${azurerm_public_ip.nagios_publicip.id}"
+        public_ip_address_id          = azurerm_public_ip.nagios_publicip.id
     }
 
     #tags {
@@ -39,9 +39,9 @@ resource "azurerm_network_interface" "nagios_nic" {
 resource "azurerm_virtual_machine" "nagios" {
     name                  = "nagios"
     location              = "northeurope"
-    resource_group_name   = "${azurerm_resource_group.rg001a.name}"
-    network_interface_ids = ["${azurerm_network_interface.nagios_nic.id}"]
-    vm_size              = "${var.vmsizes}"
+    resource_group_name   = azurerm_resource_group.nagios_resourcegroup.name
+    network_interface_ids = [azurerm_network_interface.nagios_nic.id]
+    vm_size              = var.vmsizes
     
     
 #  Uncomment this line to delete the OS disk automatically when deleting the VM
@@ -66,9 +66,9 @@ resource "azurerm_virtual_machine" "nagios" {
 
     os_profile {
         computer_name  = "nagios"
-        admin_username = "${var.VMAdminName}"
-        admin_password = "${var.VMAdminPassword}"
-        #custom_data = "${file("${path.root}${var.CloudinitscriptPath}")}"
+        admin_username = var.VMAdminName
+        admin_password = var.VMAdminPassword
+        #custom_data = file(path.rootvar.CloudinitscriptPath)
     }
 
     os_profile_linux_config {
@@ -77,7 +77,7 @@ resource "azurerm_virtual_machine" "nagios" {
 
     boot_diagnostics {
         enabled = "true"
-        storage_uri = "${azurerm_storage_account.nagios_storageaccount.primary_blob_endpoint}"
+        storage_uri = azurerm_storage_account.nagios_storageaccount.primary_blob_endpoint
     }
 
     tags {
@@ -85,5 +85,5 @@ resource "azurerm_virtual_machine" "nagios" {
     }
 }
 output "ipNAGIOS" {
-  value = "${azurerm_public_ip.nagios_publicip.ip_address}"
+  value = azurerm_public_ip.nagios_publicip.ip_address
 }
