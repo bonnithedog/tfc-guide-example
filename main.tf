bc4bd91c2cd5b24990f3b9d5b53f0d0a2b7c6047
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-24 19:41:07 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ provider "azurerm" {
 
   subscription_id = var.subscription_id
   client_id       = var.client_id
-  client_secret   = var.client_secret
+  client_secret   = var.ARM_CLIENT_SECRET
   tenant_id       = var.tenant_id
 
   features {}
