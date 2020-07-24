84e0a8a8c71574e84fc6c2245bdf683175fb6a87
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-24 20:54:55 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "nw001" {
-  name                = "example-network"
+  name                = "nw001-network"
   resource_group_name = azurerm_resource_group.rg001a.name
   location            = azurerm_resource_group.rg001a.location
   address_space       = ["10.0.0.0/16"]
