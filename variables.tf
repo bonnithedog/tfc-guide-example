8394372639d939e0c000637cf88adf5f77f3624d
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-24 20:30:16 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -43,14 +43,5 @@ variable "azure-tenant-id" {
     }
 
 
-# Generate random text for a unique storage account name
-resource "random_id" "randomId" {
-    keepers = {
-        # Generate a new ID only when a new resource group is defined
-        resource_group = "${azurerm_resource_group.nagios_resourcegroup.name}"
-    }
-
-    byte_length = 8
-}
 
 
