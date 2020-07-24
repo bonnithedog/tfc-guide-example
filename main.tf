90a648840b1cde077ee5ee66ef4f534eb6ab4645
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-24 19:07:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,8 +26,8 @@
 #    user_name = var.tag_user_name
 #  }
 #}
-variable "client_secret" {
-}
+#variable "client_secret" {
+#}
 
 provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
