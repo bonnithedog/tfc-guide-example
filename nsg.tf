fabf5eb4b940e2f8d279faf0aa7da541271afa3d
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-24 21:34:50 +0200

    Delete nsg.tf

--- nsg.tf
+++
@@ -1,7 +0,0 @@
-# Create Network Security Group and rule
-resource "azurerm_network_security_group" "nagios_sg" {
-    name                = "nagios_sg"
-    location            = "northeurope"
-    resource_group_name = "${azurerm_resource_group.rg001a.name}"
-
-  
