8b6facb2a46f6765bd47e8d20a15203ec68f296a
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-24 20:29:15 +0200

    Delete resourcegroup.tf

--- resourcegroup.tf
+++
@@ -1,9 +0,0 @@
-# Create a resource group if it doesn’t exist
-resource "azurerm_resource_group" "nagios_resourcegroup" {
-    name     = "nagios_ResourceGroup"
-    location = "northeurope"
-
-    tags {
-        environment = "NAGIOS"
-    }
-}
