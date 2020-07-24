f4d4a9d7157628944007a58ce98d91eb075829e0
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-24 21:21:32 +0200

    Update nagiosinfra.tf

--- nagiosinfra.tf
+++ nagiosinfra.tf
@@ -27,7 +27,6 @@ resource "azurerm_network_security_group" "nagios_sg" {
     name                = "nagios_sg"
     location            = "northeurope"
     resource_group_name = azurerm_resource_group.rg001a.name
-
-    
+}
 
     
