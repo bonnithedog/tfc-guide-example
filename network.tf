a6f8274d48296d9f26735b3dca260d234228cb54
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-24 21:08:08 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -5,3 +5,16 @@ resource "azurerm_virtual_network" "nw001" {
   location            = azurerm_resource_group.rg001a.location
   address_space       = ["10.0.0.0/16"]
 }
+
+
+# Create public IPs
+resource "azurerm_public_ip" "nagios_publicip" {
+    name                         = "nagios_PublicIP"
+    location                     = "northeurope"
+    resource_group_name          = "azurerm_resource_group.rg001a.name
+    allocation_method            = "Static"
+
+    tags {
+        environment = "NAGIOS"
+    }
+}
