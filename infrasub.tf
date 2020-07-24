0da0f368f9f9f602c819ba8b4089634ca88f8d24
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-24 20:45:14 +0200

    Update infrasub.tf

--- infrasub.tf
+++ infrasub.tf
@@ -13,24 +13,4 @@ resource "azurerm_resource_group" "nagios_resourcegroup" {
     }
 }
 
-# Create virtual network
-resource "azurerm_virtual_network" "nagios_vnet" {
-    name                = "nagios_vnet"
-    address_space       = ["16.0.0.0/8"]
-    location            = "northeurope"
-    resource_group_name = "${azurerm_resource_group.nagios_resourcegroup.name}"
-
-    tags {
-        environment = "NAGIOS"
-    }
-}
-
-# Create subnet
-resource "azurerm_subnet" "nagios_subnet" {
-    name                 = "nagios_subnet"
-    resource_group_name  = "${azurerm_resource_group.nagios_resourcegroup.name}"
-    virtual_network_name = "${azurerm_virtual_network.nagios_vnet.name}"
-    address_prefix       = "16.180.0.0/24"
-}
-
 
