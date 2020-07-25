7136e73eec3278d2b6a3185a29cdc38f5b6534fc
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-25 06:35:59 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,10 +5,10 @@ provider "azurerm" {
   
 
 
-  subscription_id = var.azure-subscription-id
-  client_id       = var.azure-client-id
-  client_secret   = var.azure-client-secret 
-  tenant_id       = var.azure-tenant-id
+ #subscription_id = var.azure-subscription-id
+ #client_id       = var.azure-client-id
+ #client_secret   = var.azure-client-secret 
+ #tenant_id       = var.azure-tenant-id
 
   features {}
 }
